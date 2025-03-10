e1198ab1668a3c2ce23fcb6d5d8b9a747f2b05a1
MercedesW <mercedesw13@protonmail.com>
2025-03-10 18:44:04 -0300

    setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # js-game-dev
+
+Seguimiento del tutorial: https://www.youtube.com/watch?v=U34l-Xz5ynU&list=WL&index=98&t=14s
+
+JavaScript GameDev Tutorial – Code an Animated Physics Game [Full Course]
---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1280;
+    canvas.height = 720;
+})
