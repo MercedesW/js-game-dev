8018cf1a311bb5e335deda9fe9eec537b389bef7
MercedesW <mercedesw13@protonmail.com>
2025-03-11 17:38:38 -0300

    agrega los métodos draw y render para mostrar el player en el canvas

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ window.addEventListener('load', function() {
         constructor(game) {
             this.game = game;
         }
+
+        draw(context) {
+            /* https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/beginPath */
+            context.beginPath();
+            /* https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc */
+            // arc(x, y, radius, startAngle(en rad), endAngle(en rad), counterclockwise)
+            // Math.PI * 2 hace un círculo entero
+            context.arc(400, 500, 50, 0, Math.PI * 2);
+            context.fill();
+        }
     }
 
     class Game {
@@ -21,9 +31,13 @@ window.addEventListener('load', function() {
             /* https://www.escuelafrontend.com/valor-y-referencia-en-javascript */
             this.player = new Player(this);
         }
+        render(context) {
+            this.player.draw(context);
+        }
     }
 
     const game = new Game(canvas);// le pasa el canvas de la línea 3
+    game.render(ctx); // línea 2
     console.log(game)
     // El orden de las clases importa
     // en la consola hay una recursión donde game tiene player y ese player tiene el game, etc( no sé si está bien)
